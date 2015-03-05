7e418de8312ba5d96aec7d06aba01221c11bb39a
talhaobject90 <talha@object90.com>
2015-03-05 11:58:49 +0530

    sdf

--- hungry/settings.py
+++ hungry/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mathfilters',
     'hungry'
     
     #'beachnbeach.templatetags.resort_location'
